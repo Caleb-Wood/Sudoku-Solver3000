f1230cf022d168d1631586bf5712a3b5788c4ee2
Caleb-Wood <96460748+Caleb-Wood@users.noreply.github.com>
2022-03-31 15:54:01 +0530

    first sudoku solver

---
+++ src/App.java
@@ -0,0 +1,88 @@
+public class App {
+    static char[][] board = {
+            { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+            { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+            { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+            { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+            { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+            { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+            { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+            { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+            { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+    };
+
+    //output
+    public static void main(String[] args) {
+        char[][] solvedSudoku = App.solveSudoku(board);
+        System.out.println(solvedSudoku);
+    }
+
+    // checking if number is safe to place or not
+    public static boolean isSafe(char[][] board, int row, int col, int number) {
+        // row & column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == (char) (number + '0')) {
+                return false;
+            }
+            if (board[row][i] == (char) (number + '0')) {
+                return false;
+            }
+        }
+
+        // grid
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // main solver
+    public static char[][] helper(char[][] board, int row, int col) {
+        if (row == board.length) {
+            return board;
+        }
+
+        int nrow = 0;
+        int ncol = 0;
+
+        if (col == board.length - 1) {
+            nrow = row + 1;
+            ncol = 0;
+        } else {
+            nrow = row;
+            ncol = col + 1;
+        }
+
+        if (board[row][col] != '.') {
+            if (helper(board, nrow, ncol) != null) {
+                return board;
+            }
+        } else {
+
+            // fill the place
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (helper(board, nrow, ncol) != null)
+                        return board;
+                    else
+                        board[row][col] = '.';
+                }
+            }
+        }
+
+        return board;
+    }
+
+    public static char[][] solveSudoku(char[][] board) {
+        return helper(board, 0, 0);
+    }
+
+}
